81979fa137f73ec2e9d39088f4a8bb0e4353fd7a
Nick Alexeev <nickalexeev@protonmail.com>
2020-06-17 20:28:14 -0800

    reorganized project

---
+++ renderer/src/main.rs
@@ -0,0 +1,11 @@
+mod lib;
+
+use glutin::event::{Event, WindowEvent};
+use glutin::event_loop::{ControlFlow, EventLoop};
+use glutin::window::WindowBuilder;
+use glutin::ContextBuilder;
+
+fn main() {
+    lib::init();
+    
+}
